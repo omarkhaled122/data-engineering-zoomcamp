76890d268c1763653658bc0c5aca52bf1280b887
Omar Elsheikh <omarelsheikh12.ok@gmail.com>
2025-06-03 10:58:16 +0000

    payment macro and green_tripdata id column

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -9,6 +9,7 @@ source as (
 renamed as (
 
     select
+        {{ dbt_utils.generate_surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as trip_id,
         vendorid,
         lpep_pickup_datetime,
         lpep_dropoff_datetime,
@@ -26,6 +27,7 @@ renamed as (
         improvement_surcharge,
         total_amount,
         payment_type,
+        {{get_payment_type_description('payment_type')}} AS payment_type_description,
         trip_type,
         congestion_surcharge,
         ehail_fee
